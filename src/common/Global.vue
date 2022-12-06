0e58727f6cbf1276253c97b810a8d92bb916106e
JHZZ_mjz <m2562771185@163.com>
2022-12-06 16:08:08 +0800

    第一次提交

---
+++ src/common/Global.vue
@@ -0,0 +1,11 @@
+<script>
+// const host = 'http://192.168.2.8:8888/yc/sso'
+// const fileaddr = 'http://192.168.2.8:8888/yc/static/file'
+//本地
+const host = 'http://localhost:8081/yc/sso/cl'
+const fileaddr = 'http://localhost:8081/yc/static/cl'
+export default {
+  host,
+  fileaddr,
+}
+</script>
